cdeca4bceddd02b9ab193192be024224e82e72cb
csarsnchz <csarsnchz@gmail.com>
2024-04-15 22:49:19 -0600

    Add TypeOrmModule import and User entity to AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { User } from './entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import e from 'express';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+  ],
+  exports: [TypeOrmModule],
 })
 export class AuthModule {}
--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,23 @@
-export class CreateAuthDto {}
+import { IsArray, IsBoolean, IsEmail, IsOptional, IsString, MinLength } from "class-validator";
+
+export class CreateAuthDto {
+    @IsString()
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @MinLength(6)
+    password: string;
+
+    @IsString()
+    @IsOptional()
+    @MinLength(10)
+    fullName: string;
+
+    @IsBoolean()
+    isActive: boolean;
+
+    @IsString({ each: true })
+    @IsArray()
+    roles: string[];
+}

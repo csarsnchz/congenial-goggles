8af60c39359b61ee3a4416e78f6d1f77206a62ea
csarsnchz <csarsnchz@gmail.com>
2024-04-21 19:53:48 -0600

    Refactor auth controller and service to use LoginUserDto for login functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import { LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -24,6 +25,26 @@ export class AuthService {
     } catch (error) {
       this.handleErrors(error);
     }
+  }
+
+  async login(loginUserDto: LoginUserDto){
+    try {
+      const { password, email } = loginUserDto;
+      const user = await this.userRepository.findOne({ 
+        where: {email},
+        select: {email: true, password: true}
+      });
+      if (!user) {
+        throw new UnauthorizedException('Invalid email');
+      }
+      if (!bcrypt.compareSync(password, user.password)) {
+        throw new UnauthorizedException('Invalid password');
+      }
+      return user;
+    } catch (error) {
+      this.handleErrors(error);
+    }
+  
   }
   
   private handleErrors(error: any) {
